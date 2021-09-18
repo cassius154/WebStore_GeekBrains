304f6b3d00e6ecd1fc5838b3ba513abcbc9ce51d
Даже доктор недоволен тобой <cassius154@users.noreply.github.com>
2021-09-18 18:27:16 +0300

    Сервисы MVC и настройка роутинга

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -24,6 +24,7 @@ namespace WebStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,10 +39,15 @@ namespace WebStore
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
+                endpoints.MapGet("/greetings", async context =>
                 {
                     await context.Response.WriteAsync(Configuration["Greeting"]);
                 });
+
+                //endpoints.MapDefaultControllerRoute();
+                endpoints.MapControllerRoute(
+                    "default",
+                    "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }

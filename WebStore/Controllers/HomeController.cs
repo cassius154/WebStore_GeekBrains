b477afcee8f318adb37c8999b7b475dc62c40d5b
Даже доктор недоволен тобой <cassius154@users.noreply.github.com>
2021-09-18 19:29:17 +0300

    Представление с сотрудниками

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly List<Employee> _employees = new ()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", BirthDate = new DateTime(1985, 03, 23) },
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Петр", Patronymic = "Петрович", BirthDate = new DateTime(1995, 07, 27) },
+            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", BirthDate = new DateTime(2001, 11, 21) },
+        };
+
         public IActionResult Index()
         {
             return View();
@@ -12,6 +22,11 @@ namespace WebStore.Controllers
         public IActionResult SecondAction(int id)
         {
             return Content($"Hello from SecondAction with id = {id}!");
+        }
+
+        public IActionResult Employees()
+        {
+            return View(_employees);
         }
     }
 }

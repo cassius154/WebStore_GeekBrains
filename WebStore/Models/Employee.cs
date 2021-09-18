6061d1549d0ae54910855fd4fb30ecaf4f5c2920
Даже доктор недоволен тобой <cassius154@users.noreply.github.com>
2021-09-18 19:28:55 +0300

    Модель сотрудника

---
+++ WebStore/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace WebStore.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public int Age 
+        {
+            get 
+            {
+                DateTime now = DateTime.Today;
+                int age = now.Year - BirthDate.Year;
+                if (BirthDate > now.AddYears(-age)) age--;
+                return age;
+            }
+        }
+    }
+
+    //public record Employee2(int id, string FirstName, string LastName, string Patronymic, DateTime BirthDate);
+}

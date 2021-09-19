75d7a0fa479bca7b6ed3a045dacc58d855a48031
Даже доктор недоволен тобой <cassius154@users.noreply.github.com>
2021-09-20 02:23:02 +0300

    CRUD-операции с сотрудником

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -3,19 +3,23 @@ using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using WebStore.Data;
 using WebStore.Models;
+using WebStore.Services;
 
 namespace WebStore.Controllers
 {
     public class EmployeesController : Controller
     {
-        private readonly IEnumerable<Employee> _employees;
+        //private readonly IEnumerable<Employee> _employees;
+
+        private readonly EmployeeService _empService;
 
         public EmployeesController()
         {
-            _employees = TestData.Employees;
+            //_employees = TestData.Employees;
+            _empService = new EmployeeService();
         }
 
-        public IActionResult Index() => View(_employees);
+        public IActionResult Index() => View(_empService.GetEmployeeList());
 
         public IActionResult Details(int? id)
         {
@@ -24,13 +28,88 @@ namespace WebStore.Controllers
                 return NotFound();
             }
 
-            var emp = _employees.SingleOrDefault(e => e.Id == id);
+            var emp = _empService.GetEmployee(id.Value);
             if (emp is null)
             {
                 return NotFound();
             }
 
             return View(emp);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id is null)
+            {
+                return NotFound();
+            }
+
+            var emp = _empService.GetEmployee(id.Value);
+            if (emp is null)
+            {
+                return NotFound();
+            }
+
+            return View(emp);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Employee model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var emp = _empService.Edit(model);
+            if (emp is null)
+            {
+                return NotFound();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public IActionResult Create(Employee model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            _empService.Add(model);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id is null)
+            {
+                return NotFound();
+            }
+
+            var emp = _empService.GetEmployee(id.Value);
+            if (emp is null)
+            {
+                return NotFound();
+            }
+
+            return View(emp);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int? id)
+        {
+            if (id != null)
+            {
+                _empService.Delete(id.Value);
+            }
+            return RedirectToAction("Index");
         }
     }
 }

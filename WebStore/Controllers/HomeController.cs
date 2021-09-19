295fad57f8f3e4a82ddc93323cfa35660beb1170
Даже доктор недоволен тобой <cassius154@users.noreply.github.com>
2021-09-19 23:44:18 +0300

    Оживил все ссылки на всех страницах

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -6,5 +6,26 @@ namespace WebStore.Controllers
     {
         public IActionResult Index() => View();
 
+        public IActionResult Blog() => View();
+
+        public IActionResult BlogSingle() => View();
+
+        public IActionResult Cart() => View();
+
+        public IActionResult Checkout() => View();
+
+        public IActionResult ContactUs() => View();
+
+        public IActionResult Login() => View();
+
+        public IActionResult ProductDetails() => View();
+
+        public IActionResult Shop() => View();
+
+        public IActionResult Account() => View();
+
+        public IActionResult Dreams() => View();
+
+        public IActionResult NotFoundPage() => View();
     }
 }

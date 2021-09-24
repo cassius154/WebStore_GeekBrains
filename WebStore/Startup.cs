2c1ff8fd5f1ee5cc997cbd10d803afe1b579cf39
AlexK <cassius154@users.noreply.github.com>
2021-09-24 12:30:22 +0300

    Добавлен пакет BrowserLink (связь с браузером)

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -38,6 +38,7 @@ namespace WebStore
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
             }
 
             //внутри ответа со статусным кодом должен быть адрес страницы, чтобы браузер на нее перешел

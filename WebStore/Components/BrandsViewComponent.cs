b9e92fb182cd5a9563b2b2a405f81405b08d8c1b
AlexK <cassius154@users.noreply.github.com>
2021-09-24 16:31:36 +0300

    Заготовки визуальных компонент

---
+++ WebStore/Components/BrandsViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Components
+{
+    //[ViewComponent(Name = "BrandsVvv")]  - вместо наследования можно применить этот атрибут
+    public class BrandsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke() => View();
+
+        //public async Task<IViewComponentResult> InvokeAsync() => View();  //можно и асинхронный 
+    }
+}
---
+++ WebStore/Components/SectionsViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Components
+{
+    public class SectionsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke() => View();
+
+        //public async Task<IViewComponentResult> InvokeAsync() => View();  //можно и асинхронный 
+    }
+}

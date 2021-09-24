a05a5c57b2bd96579959172d86410841518ac966
AlexK <cassius154@users.noreply.github.com>
2021-09-24 17:23:48 +0300

    Логика визуального компонента секций

--- WebStore/Components/SectionsViewComponent.cs
+++ WebStore/Components/SectionsViewComponent.cs
@@ -1,10 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.DTO;
+using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Components
 {
     public class SectionsViewComponent : ViewComponent
     {
-        public IViewComponentResult Invoke() => View();
+        private readonly IProductData _prodData;
+
+        public SectionsViewComponent(IProductData prodData)
+        {
+            _prodData = prodData;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var sections = _prodData.GetSections();
+            var parents = sections.Where(s => s.ParentId is null);
+
+            var parentViews = parents
+                .Select(p => new SectionViewModel
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Order = p.Order,
+                })
+                .ToList();
+
+            foreach(var pv in parentViews)
+            {
+                var childs = sections.Where(s => s.ParentId == pv.Id);
+
+                foreach (var child in childs)
+                {
+                    pv.Childs.Add(new SectionViewModel
+                    {
+                        Id = child.Id,
+                        Name = child.Name,
+                        Order = child.Order,
+                        Parent = pv,
+                    });
+                }
+
+                pv.Childs.Sort((a, b) => Comparer<int>.Default.Compare(a.Order, b.Order));
+            }
+
+            parentViews.Sort((a, b) => Comparer<int>.Default.Compare(a.Order, b.Order));
+
+            return View(parentViews);
+        }
 
         //public async Task<IViewComponentResult> InvokeAsync() => View();  //можно и асинхронный 
     }

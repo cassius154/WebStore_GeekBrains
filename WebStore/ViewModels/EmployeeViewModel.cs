5592639ed538ff40f65bf0331ac996c99e222e6d
AlexK <cassius154@users.noreply.github.com>
2021-09-24 14:38:28 +0300

    Варианты валидации

--- WebStore/ViewModels/EmployeeViewModel.cs
+++ WebStore/ViewModels/EmployeeViewModel.cs
@@ -1,20 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebStore.ViewModels
 {
-    public class EmployeeViewModel
+    public class EmployeeViewModel : IValidatableObject  //кроме атрибутов можно реализовать IValidatableObject
     {
         [HiddenInput(DisplayValue = false)]
         public int Id { get; set; }
 
         [Display(Name = "Имя")]
         [Required(ErrorMessage = "Имя обязательно к заполнению")]
+        [StringLength(200, MinimumLength = 2, ErrorMessage = "Длина строки должна быть от 2 до 200 символов")]
+        [RegularExpression(@"([А-ЯЁ][а-яё]+)|([A-Z][a-z]+)", ErrorMessage = "Неверный формат")]  //Первая буква - заглавная, дальше строчные, при этом кириллица и латиница не смешиваются
         public string FirstName { get; set; }
 
         [Display(Name = "Фамилия")]
         [Required(ErrorMessage = "Фамилия обязательна к заполнению")]
+        [StringLength(200, MinimumLength = 2, ErrorMessage = "Длина строки должна быть от 2 до 200 символов")]
+        [RegularExpression(@"([А-ЯЁ][а-яё]+)|([A-Z][a-z]+)", ErrorMessage = "Неверный формат")]  //Первая буква - заглавная, дальше строчные, при этом кириллица и латиница не смешиваются
         public string LastName { get; set; }
 
         [Display(Name = "Отчество")]
@@ -29,6 +34,7 @@ namespace WebStore.ViewModels
         public DateTime? BirthDate { get; set; }
 
         [Display(Name = "Возраст")]
+        //[Range(18, 80, ErrorMessage = "Возраст должен быть от 18 до 80")]  //для числовых можно использовать атрибут RangeAtribute
         public int Age
         {
             get
@@ -37,6 +43,27 @@ namespace WebStore.ViewModels
                 int age = dtNow.Year - BirthDate.Value.Year;
                 if (BirthDate > dtNow.AddYears(-age)) age--;
                 return age;
+            }
+        }
+
+
+        //IValidatableObject
+        //не работает - validationContext.MemberName приходит "model" - разобраться потом
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            return new[] { ValidationResult.Success };
+
+            switch (validationContext.MemberName)
+            {
+                case nameof(Age):
+                    if (Age < 15 && Age > 80)
+                    {
+                        return new[] { new ValidationResult("Странный возраст", new[] { nameof(Age) }) };
+                    }
+                    return new[] { ValidationResult.Success };
+
+                default: return new[] { ValidationResult.Success };
+
             }
         }
     }

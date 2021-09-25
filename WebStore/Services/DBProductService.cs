3c45f1019ce5dc09ac0a5633e387ffb3c1a16f48
AlexK <cassius154@users.noreply.github.com>
2021-09-25 11:14:02 +0300

    Реализация ProductService через БД

---
+++ WebStore/Services/DBProductService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Data;
+using WebStore.Domain;
+using WebStore.Domain.Entities;
+using WebStore.Services.Interfaces;
+
+namespace WebStore.Services
+{
+    public class DBProductService : IProductService
+    {
+        private WebStoreDbContext _db;
+
+        public DBProductService(WebStoreDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Brand> GetBrands()
+        {
+            return _db.Brands.ToList();
+        }
+
+        public IEnumerable<Product> GetProducts(ProductFilter filter = null)
+        {
+            IQueryable<Product> ret = _db.Products;
+            if (filter?.BrandId is { } brandId)
+            {
+                ret = ret.Where(p => p.BrandId == brandId);
+            }
+
+            if (filter?.SectionId is not null)
+            {
+                ret = ret.Where(p => p.SectionId == filter.SectionId);
+            }
+
+            return ret.ToList();
+        }
+
+        public IEnumerable<Section> GetSections()
+        {
+            return _db.Sections.ToList();
+        }
+    }
+}

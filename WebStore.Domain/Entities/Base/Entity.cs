5df0c436052544c94ac428c6c9b7400355c1778b
AlexK <cassius154@users.noreply.github.com>
2021-09-28 15:15:45 +0300

    Атрибуты у сущностей, помогающие в создании структуры

--- WebStore.Domain/Entities/Base/Entity.cs
+++ WebStore.Domain/Entities/Base/Entity.cs
@@ -1,9 +1,15 @@
-﻿using WebStore.Domain.Entities.Base.Interfaces;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using WebStore.Domain.Entities.Base.Interfaces;
 
 namespace WebStore.Domain.Entities.Base
 {
     public abstract class Entity : IEntity
     {
+        //эти 2 атрибута в данном случае необязательны
+        //при имени поля Id (или <ClassName>Id) - все это EF сделает сама
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
     }
 }
--- WebStore.Domain/Entities/Base/NamedEntity.cs
+++ WebStore.Domain/Entities/Base/NamedEntity.cs
@@ -1,9 +1,13 @@
-﻿using WebStore.Domain.Entities.Base.Interfaces;
+﻿using System.ComponentModel.DataAnnotations;
+using WebStore.Domain.Entities.Base.Interfaces;
 
 namespace WebStore.Domain.Entities.Base
 {
+    //[Index(nameof(Name)] - можно здесь, для всех потомков - а можно у каждого в отдельности.
+    //Зависит от того, будут ли индексы отличаться - например, уникальностью
     public abstract class NamedEntity : Entity, INamedEntity
     {
+        [Required]
         public string Name { get; set; }
     }
 }

f22d74e9c47988c91348a7008b8d75478d224306
AlexK <cassius154@users.noreply.github.com>
2021-09-28 19:32:33 +0300

    Исправление инициализации БД первоначальными данными

--- WebStore/Data/WebStoreDbInitializer.cs
+++ WebStore/Data/WebStoreDbInitializer.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using WebStore.DAL.Context;
 
 namespace WebStore.Data
@@ -10,11 +9,18 @@ namespace WebStore.Data
     public class WebStoreDbInitializer
     {
         private readonly WebStoreDbContext _db;
+        private readonly ILogger<WebStoreDbInitializer> _logger;
 
-        public WebStoreDbInitializer(WebStoreDbContext db) => _db = db;
+        public WebStoreDbInitializer(WebStoreDbContext db, ILogger<WebStoreDbInitializer> logger)
+        {
+            _db = db;
+            _logger = logger;
+        }
 
         public async Task InitializeAsync()
         {
+            _logger.LogInformation("Запуск инициализации БД");
+
             //var deleted = await _db.Database.EnsureDeletedAsync();  //если надо удалить БД при старте
             //var created = await _db.Database.EnsureCreatedAsync(); //целесообразно вызывать, если нет миграций
 
@@ -26,6 +32,7 @@ namespace WebStore.Data
 
             if (pendingMigrations.Any())
             {
+                _logger.LogInformation($"Применение миграций: {string.Join(",", pendingMigrations)}");
                 await _db.Database.MigrateAsync();
             }
 
@@ -34,24 +41,36 @@ namespace WebStore.Data
 
         private async Task _initProductsAsync()
         {
+            if (_db.Products.Any())
+            {
+                _logger.LogInformation("Инициализация БД нформацией о товарах не требуется.");
+                return;
+            }
+
             //await using (var tx = await _db.Database.BeginTransactionAsync())
             await using (await _db.Database.BeginTransactionAsync())
             {
+                _logger.LogInformation("Добавление секций...");
                 await _db.Sections.AddRangeAsync(TestData.Sections);
                 //отключаем генерацию ID на сервере - вставляем свои из TestData
                 await _db.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT [dbo].[Sections] ON");
                 await _db.SaveChangesAsync();
                 await _db.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT [dbo].[Sections] OFF");
+                _logger.LogInformation("Добавление секций выполнено успешно");
 
+                _logger.LogInformation("Добавление брендов...");
                 await _db.Brands.AddRangeAsync(TestData.Brands);
                 await _db.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT [dbo].[Brands] ON");
                 await _db.SaveChangesAsync();
                 await _db.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT [dbo].[Brands] OFF");
+                _logger.LogInformation("Добавление брендов выполнено успешно");
 
+                _logger.LogInformation("Добавление товаров...");
                 await _db.Products.AddRangeAsync(TestData.Products);
                 await _db.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT [dbo].[Products] ON");
                 await _db.SaveChangesAsync();
                 await _db.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT [dbo].[Products] OFF");
+                _logger.LogInformation("Добавление товаров выполнено успешно");
 
                 //await tx.CommitAsync();
                 await _db.Database.CommitTransactionAsync();

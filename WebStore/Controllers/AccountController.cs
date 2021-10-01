00de794c0b14af20eff9e6ea8e5ccb3a35d3c7b5
AlexK <cassius154@users.noreply.github.com>
2021-10-01 15:40:56 +0300

    Заготовка контроллера Account и его представлений

---
+++ WebStore/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        public async Task<IActionResult> Register()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> AccessDenied()
+        {
+            return View();
+        }
+    }
+}

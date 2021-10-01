34086d6e418280fbd287aec072ba4c56380216a1
AlexK <cassius154@users.noreply.github.com>
2021-10-01 13:29:27 +0300

    Модели пользоателя и роли

---
+++ WebStore.Domain/Identity/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebStore.Domain.Identity
+{
+    public class User : IdentityUser
+    {
+        public const string AdmLogin = "Admin";
+
+        public const string DefaultAdmPassword = "AdPass_123";
+    }
+}

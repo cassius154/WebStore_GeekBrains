572ed722b135e86ff9e0cd5a2a91bc5e05ab2c7e
AlexK <cassius154@users.noreply.github.com>
2021-10-01 16:39:49 +0300

    Регистрация нового пользователя

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WebStore.Domain.Identity;
+using WebStore.ViewModels.Identity;
 
 namespace WebStore.Controllers
 {
@@ -16,22 +17,50 @@ namespace WebStore.Controllers
             _signInManager = signInManager;
         }
 
-        public async Task<IActionResult> Register()
-        {
-            return View();
-        }
+        #region Register
+        public IActionResult Register() => View();
 
-        public async Task<IActionResult> Login()
+        //AntiForgeryToken - к форме прикрепляется некая информация, которую заранее передает сервер
+        //(в методе GET, если указать во вьюхе на форме атрибут у tag-helpera form asp-antiforgery="true")
+        //потом этот токен возвращается с формой в POST - и можно подтвердить, что не было подмены агента (браузера)
+        //не было перехвата информации и ее подмены
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterUserViewModel model)
         {
-            return View();
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = new User
+            {
+                UserName = model.UserName,
+            };
+
+            var regResult = await _userManager.CreateAsync(user, model.Password);
+            if (regResult.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false); //false - непостоянный, разовый вход
+                return RedirectToAction("Index", "Home");
+            }
+
+            foreach (var error in regResult.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+            return View(model);
         }
+        #endregion Register
+
+        public IActionResult Login() => View();
 
         public async Task<IActionResult> Logout()
         {
-            return View();
+            
+            return RedirectToAction("Index", "Home");
         }
 
-        public async Task<IActionResult> AccessDenied()
+        public IActionResult AccessDenied()
         {
             return View();
         }

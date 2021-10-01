759d3172f592b54ccc62dc02b305142710f6d940
AlexK <cassius154@users.noreply.github.com>
2021-10-01 14:57:13 +0300

    Конфигурация подключения Identity

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -1,12 +1,15 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using WebStore.DAL.Context;
 using WebStore.Data;
+using WebStore.Domain.Identity;
 using WebStore.Infrastructure.Conventions;
 using WebStore.Infrastructure.Middleware;
 using WebStore.Services.Interfaces;
@@ -28,6 +31,48 @@ namespace WebStore
         {
             services.AddDbContext<WebStoreDbContext>(opt => 
                 opt.UseSqlServer(Configuration.GetConnectionString("WebStoreSql")));
+
+            services.AddIdentity<User, Role>(/*opt => { opt. }*/)  //можно сконфигурить прямо тут
+                .AddEntityFrameworkStores<WebStoreDbContext>()    //указываем, каким способом хранить - посредством EF
+                .AddDefaultTokenProviders();    //это пока неясно что
+            //конфигурим Identity
+            services.Configure<IdentityOptions>(opt => 
+            {
+#if true
+                //для отладочных целей ослабляем требования к паролю
+                opt.Password.RequireDigit = false;
+                opt.Password.RequireLowercase = false;
+                opt.Password.RequireUppercase = false;
+                opt.Password.RequireNonAlphanumeric = false;
+                opt.Password.RequiredLength = 3;
+                opt.Password.RequiredUniqueChars = 1;
+#endif
+                opt.User.RequireUniqueEmail = false;
+                opt.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+                opt.Lockout.AllowedForNewUsers = false;
+                opt.Lockout.MaxFailedAccessAttempts = 10;
+                opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
+            });
+
+            //когда юзер залогинился система Identity сохраняет факт входа в кукис
+            //конфигурим кукис
+            services.ConfigureApplicationCookie(opt => 
+            {
+                opt.Cookie.Name = "GB.WebStore";
+                opt.Cookie.HttpOnly = true;  //повышает безопасность, отключая все другие каналы передачи кукисов, кроме http
+
+                opt.ExpireTimeSpan = TimeSpan.FromDays(10);
+
+                opt.LoginPath = new Microsoft.AspNetCore.Http.PathString("/Account/Login"); //"/Account/Login";
+                opt.LogoutPath = "/Account/Logout";
+                opt.AccessDeniedPath = "/Account/AccessDenied";
+
+                //незалогиненным юзерам Identity все равно присваивает идентификатор (анонимуса), который существует в сеансе, и сохраняет его в кукисе
+                //после логина этот анонимный id надо сменить на id вошедшего юзера, иначе существуют атаки для увода данных
+                //этот параметр и отвечает за принудительную смену анонимного id на правильный при логине
+                opt.SlidingExpiration = true;
+            });
 
             services.AddTransient<WebStoreDbInitializer>();
 

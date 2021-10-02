e50d8d48116ace90f20cd30ddf7f0e882803c70a
AlexK <cassius154@users.noreply.github.com>
2021-10-02 13:39:29 +0300

    Создание роли админа, самого админа и добавление админа в роль

--- WebStore/Data/WebStoreDbInitializer.cs
+++ WebStore/Data/WebStoreDbInitializer.cs
@@ -1,9 +1,12 @@
 ﻿using System.Diagnostics;
+using System.IO.Pipelines;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using WebStore.DAL.Context;
+using WebStore.Domain.Identity;
 
 namespace WebStore.Data
 {
@@ -11,11 +14,18 @@ namespace WebStore.Data
     {
         private readonly WebStoreDbContext _db;
         private readonly ILogger<WebStoreDbInitializer> _logger;
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<Role> _roleManager;
 
-        public WebStoreDbInitializer(WebStoreDbContext db, ILogger<WebStoreDbInitializer> logger)
+        public WebStoreDbInitializer(WebStoreDbContext db,
+            ILogger<WebStoreDbInitializer> logger,
+            UserManager<User> userManager,
+            RoleManager<Role> roleManager)
         {
             _db = db;
             _logger = logger;
+            _userManager = userManager;
+            _roleManager = roleManager;
         }
 
         public async Task InitializeAsync()
@@ -38,6 +48,8 @@ namespace WebStore.Data
             }
 
             await _initProductsAsync();
+
+            await _initAdministrators();
         }
 
         private async Task _initProductsAsync()
@@ -55,7 +67,7 @@ namespace WebStore.Data
             var dictBrands = TestData.Brands.ToDictionary(b => b.Id);
 
             //проставляем секциям целиком parent-ы вместо ParentId 
-            foreach(var childSection in TestData.Sections.Where(s => s.ParentId is not null))
+            foreach (var childSection in TestData.Sections.Where(s => s.ParentId is not null))
             {
                 childSection.Parent = dictSections[(int)childSection.ParentId!];  //! - это чтобы при какой-то включенной опции не выдавало предупреждений о непроверке на null
             }
@@ -105,5 +117,75 @@ namespace WebStore.Data
             }
             _logger.LogInformation($"Добавление каталога выполнено успешно за {timer/*ElapsedMilliseconds*/.Elapsed.TotalMilliseconds} мс");
         }
+
+        private async Task _initAdministrators()
+        {
+            IdentityResult identityResult;
+            User user = null;
+
+            var ok = await _roleManager.RoleExistsAsync(Role.Administrators);
+
+            if (!ok)
+            {
+                var role = new Role
+                {
+                    Name = Role.Administrators,
+                };
+
+                _logger.LogInformation("Создание роли администратора...");
+                identityResult = await _roleManager.CreateAsync(role);
+
+                ok = _handleResult(identityResult,
+                    "Создание роли администратора выполнено успешно.",
+                    "Ошибка создания роли администратора: {0}"
+                );
+            }
+
+            if (ok)
+            {
+                user = await _userManager.FindByNameAsync(User.AdmLogin);
+                if (user is null)
+                {
+                    user = new User
+                    {
+                        UserName = User.AdmLogin,
+                    };
+
+                    _logger.LogInformation("Создание администратора...");
+                    identityResult = await _userManager.CreateAsync(user, User.DefaultAdmPassword);
+                    ok = _handleResult(identityResult,
+                        "Создание администратора выполнено успешно.",
+                        "Ошибка создания администратора: {0}"
+                    );
+
+                    if (ok)
+                    {
+                        user = await _userManager.FindByNameAsync(User.AdmLogin);
+                    }
+                }
+            }
+
+            if (ok)
+            {
+                if (user != null && !(await _userManager.IsInRoleAsync(user, Role.Administrators)))
+                {
+                    _logger.LogInformation("Добавление администратора в группу администраторов...");
+                    identityResult = await _userManager.AddToRoleAsync(user, Role.Administrators);
+                    ok = _handleResult(identityResult,
+                        "Добавление администратора в группу администраторов выполнено успешно.",
+                        "Ошибка добавления администратора в группу администраторов: {0}"
+                    );
+                }
+            }
+        }
+
+        private bool _handleResult(IdentityResult result, string okMessage, string errMessage)
+        {
+            var ret = result.Succeeded;
+            _logger.LogInformation(ret ? okMessage : string.Format(errMessage, string.Join(", ", result.Errors.Select(e => e.Description))));
+
+            return ret;
+        }
+
     }
 }

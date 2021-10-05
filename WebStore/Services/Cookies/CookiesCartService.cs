c9bc80f3c45f643b4af87b2d8fa4a01be0aed495
AlexK <cassius154@users.noreply.github.com>
2021-10-05 17:55:54 +0300

    Основная логика сервиса корзины

--- WebStore/Services/Cookies/CookiesCartService.cs
+++ WebStore/Services/Cookies/CookiesCartService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using WebStore.Domain.Entities;
 using WebStore.Services.Interfaces;
 using WebStore.ViewModels;
 
@@ -13,6 +15,38 @@ namespace WebStore.Services.Cookies
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly IProductService _productService;
         private readonly string _cartName;
+
+        private Cart _cart
+        {
+            get  //здесь идет десериализация
+            {
+                var context = _httpContextAccessor.HttpContext;
+                var cookies = context.Response.Cookies;
+
+                var cartCookie = context.Request.Cookies[_cartName];
+                if (cartCookie is null)
+                {
+                    //если не нашли - создаем новую корзину
+                    var cart = new Cart();
+                    cookies.Append(_cartName, JsonConvert.SerializeObject(cart));  //записываем ее сериализованную в кукисы РЕСПОНЗА
+                    return cart;  //и ее же возвращаем
+                }
+
+                _replaceCart(cookies, cartCookie);  //по сути - переписываем куку из request в response
+                return JsonConvert.DeserializeObject<Cart>(cartCookie);  //и возвращаем ее десериализованную
+            }
+            set  //здесь сериализация
+            {
+                _replaceCart(_httpContextAccessor.HttpContext!.Response.Cookies, 
+                    JsonConvert.SerializeObject(value));
+            }
+        }
+
+        private void _replaceCart(IResponseCookies cookies, string cart)
+        {
+            cookies.Delete(_cartName);
+            cookies.Append(_cartName, cart);
+        }
 
         public CookiesCartService(IHttpContextAccessor httpContextAccessor, IProductService productService)
         {
@@ -26,27 +60,70 @@ namespace WebStore.Services.Cookies
 
         public void Add(int id)
         {
-            throw new NotImplementedException();
+            var cart = _cart;  //берем из входных кукисов
+
+            var item = cart.Items.FirstOrDefault(i => i.ProductId == id);
+            if (item is null)
+            {
+                cart.Items.Add(new CartItem { ProductId = id, Quantity = 1 });
+            }
+            else
+            {
+                item.Quantity++;
+            }
+
+            _cart = cart;  //записываем обратно в выходные кукисы
         }
 
         public void Clear()
         {
-            throw new NotImplementedException();
+            var cart = _cart;  //берем из входных кукисов
+            cart.Items.Clear();
+            _cart = cart;  //записываем обратно в выходные кукисы
+
+            //или можно было бы так, но возможны проблемы
+            //_cart = new();
         }
 
         public void Decrement(int id)
         {
-            throw new NotImplementedException();
-        }
+            var cart = _cart;  //берем из входных кукисов
 
-        public CartViewModel GetViewModel()
-        {
-            throw new NotImplementedException();
+            var item = cart.Items.FirstOrDefault(i => i.ProductId == id);
+            if (item is null)
+            {
+                return;
+            }
+            
+            if (item.Quantity > 0)
+            {
+                item.Quantity--;
+            }
+
+            if (item.Quantity <= 0)
+            {
+                cart.Items.Remove(item);
+            }
+
+            _cart = cart;  //записываем обратно в выходные кукисы
         }
 
         public void Remove(int id)
         {
-            throw new NotImplementedException();
+            var cart = _cart;  //берем из входных кукисов
+
+            var item = cart.Items.FirstOrDefault(i => i.ProductId == id);
+            if (item is null)
+            {
+                return;
+            }
+            cart.Items.Remove(item);
+
+            _cart = cart;  //записываем обратно в выходные кукисы
+        }
+
+        public CartViewModel GetViewModel()
+        {
         }
     }
 }

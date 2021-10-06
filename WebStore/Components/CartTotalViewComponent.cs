68e9a3afca74e523f2ed08ec4a72f99a29071295
AlexK <cassius154@users.noreply.github.com>
2021-10-06 14:53:06 +0300

    CartTotalViewComponent - убрал дублированный вызов CartService.GetViewModel и вызываю компонент простой передачей модели в Invoke

--- WebStore/Components/CartTotalViewComponent.cs
+++ WebStore/Components/CartTotalViewComponent.cs
@@ -1,24 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using WebStore.Services.Interfaces;
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.ViewModels;
 
 namespace WebStore.Components
 {
     public class CartTotalViewComponent : ViewComponent
     {
-        private readonly ICartService _cartService;
-
-        public CartTotalViewComponent(ICartService cartService)
+        public IViewComponentResult Invoke(CartViewModel model)
         {
-            _cartService = cartService;
-        }
-
-        public IViewComponentResult Invoke()
-        {
-            return View(_cartService.GetViewModel());
+            return View(model);
         }
     }
 }

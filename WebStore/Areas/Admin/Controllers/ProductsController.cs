99b964dcf198ecdcea0b301b54ff2e178c04e0fe
AlexK <cassius154@users.noreply.github.com>
2021-10-08 13:56:31 +0300

    Список товаров в контроллере управления каталогом

--- WebStore/Areas/Admin/Controllers/ProductsController.cs
+++ WebStore/Areas/Admin/Controllers/ProductsController.cs
@@ -1,17 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Services.Interfaces;
 
 namespace WebStore.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class ProductsController : Controller
     {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService) => _productService = productService;
+
         public IActionResult Index()
         {
-            return View();
+            var products = _productService.GetProducts();
+            return View(products);
         }
+
+        public IActionResult Edit(int id) => RedirectToAction(nameof(Index));
+
+        public IActionResult Delete(int id) => RedirectToAction(nameof(Index));
     }
 }

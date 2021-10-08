d4feb179b6cef67acb1cad99c3f63e2df0a06f07
AlexK <cassius154@users.noreply.github.com>
2021-10-08 15:46:06 +0300

    Интерфейс сервиса заказов

--- WebStore/Services/Interfaces/IOrderService.cs
+++ WebStore/Services/Interfaces/IOrderService.cs
@@ -1,11 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using WebStore.Domain.Entities.Orders;
+using WebStore.ViewModels;
 
 namespace WebStore.Services.Interfaces
 {
     public interface IOrderService
     {
+        Task<IEnumerable<Order>> GetUserOrder(string userName);
+
+        Task<Order> GetOrderById(int id);
+
+        Task<Order> CreateOrder(string userName, CartViewModel cart, OrderViewModel order);
     }
 }

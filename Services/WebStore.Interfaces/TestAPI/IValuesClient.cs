bde695f9546461cd07fce8cda280c3d5c92607ef
AlexK <cassius154@users.noreply.github.com>
2021-10-13 11:51:26 +0300

    Интерфейс тестового клиента WebAPI

---
+++ Services/WebStore.Interfaces/TestAPI/IValuesClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.Interfaces.TestAPI
+{
+    public interface IValuesClient
+    {
+        IEnumerable<string> GetAll();
+
+        int Count();
+
+        string GetById(int id);
+
+        void Add(string value);
+
+        void Edit(int i, string value);
+
+        bool Delete(int id);
+    }
+}

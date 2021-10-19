26ba42eac424d779200e6916d78ecaacda81512f
AlexK <cassius154@users.noreply.github.com>
2021-10-19 11:02:25 +0300

    DTO-модели для WebAPI Identity

---
+++ Common/WebStore.Domain/DTO/Identity/ClaimDTO.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace WebStore.Domain.DTO.Identity
+{
+    public abstract class ClaimDTO : UserDTO
+    {
+        public IEnumerable<Claim> Claims { get; set; }
+    }
+
+    public class AddClaimDTO : ClaimDTO { }
+
+    public class RemoveClaimDTO : ClaimDTO { }
+
+    public class ReplaceClaimDTO : UserDTO
+    {
+        //старый, заменяемый клейм
+        public Claim Claim { get; set; }
+
+        //новый клейм
+        public Claim NewClaim { get; set; }
+    }
+}

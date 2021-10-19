7074eda0a4516b95a889ccf88eee07ed26449783
AlexK <cassius154@users.noreply.github.com>
2021-10-19 20:55:41 +0300

    Документация к коду

--- Services/WebStore.WebAPI/Controllers/EmployeesApiController.cs
+++ Services/WebStore.WebAPI/Controllers/EmployeesApiController.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using WebStore.Domain.Models;
 using WebStore.Interfaces;
 using WebStore.Interfaces.Services;
 
 namespace WebStore.WebAPI.Controllers
 {
+    /// <summary>
+    /// Управление сотрудниками
+    /// </summary>
     [ApiController]
     [Route(WebAPIAddresses.Employees)]  //если оставить "api/[controller]" - адрес будет api/employeesapi, а надо api/employees
     public class EmployeesApiController : ControllerBase
@@ -16,6 +20,10 @@ namespace WebStore.WebAPI.Controllers
             _employeeService = employeeService;
         }
 
+        /// <summary>
+        /// Получение всех сотрудников
+        /// </summary>
+        /// <returns>Список сотрудников</returns>
         [HttpGet]
         public IActionResult Get()
         {
@@ -23,7 +31,15 @@ namespace WebStore.WebAPI.Controllers
             return Ok(employees);
         }
 
+        /// <summary>
+        /// Получение сотрудника по его идентификатору
+        /// </summary>
+        /// <param name="id">Идентификатор сотрудника</param>
+        /// <returns>Сотрудник с указанным идентификатором</returns>
         [HttpGet("{id}")]
+        //если метод возвращает разные статусные коды - указываем их для Swagger
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Employee))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult GetById(int id)
         {
             var employee = _employeeService.GetEmployee(id);
@@ -35,6 +51,11 @@ namespace WebStore.WebAPI.Controllers
             return Ok(employee);
         }
 
+        /// <summary>
+        /// Добавление сотрудника
+        /// </summary>
+        /// <param name="emp">Объект с данными сотрудника</param>
+        /// <returns>Созданный сотрудник</returns>
         [HttpPost]
         public IActionResult Add(Employee emp)
         {

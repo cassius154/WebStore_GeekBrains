a5fa71153f4a6f5b2af7f8bb815d9f858847e516
AlexK <cassius154@users.noreply.github.com>
2021-10-24 13:10:18 +0300

    Примеры использования ILogger<t>

--- UI/WebStore/Controllers/AccountController.cs
+++ UI/WebStore/Controllers/AccountController.cs
@@ -1,7 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.Web.CodeGeneration;
 using WebStore.Domain.Identity;
 using WebStore.ViewModels.Identity;
 
@@ -12,11 +15,16 @@ namespace WebStore.Controllers
     {
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
+        private readonly ILogger<AccountController> _logger;
 
-        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+
+        public AccountController(UserManager<User> userManager, 
+            SignInManager<User> signInManager,
+            ILogger<AccountController> logger)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _logger = logger;   // WebStore.Controllers.AccountController  - источник записи в логе будет такой
         }
 
         #region Register
@@ -37,24 +45,38 @@ namespace WebStore.Controllers
                 return View(model);
             }
 
-            var user = new User
+            using (_logger.BeginScope("Регистрация пользователя {UserName}", model.UserName))
             {
-                UserName = model.UserName,
-            };
+                var user = new User
+                {
+                    UserName = model.UserName,
+                };
 
-            var regResult = await _userManager.CreateAsync(user, model.Password);
-            if (regResult.Succeeded)
-            {
-                await _signInManager.SignInAsync(user, false); //false - непостоянный, разовый вход
-                await _userManager.AddToRoleAsync(user, Role.Users);  //добавляем сразу в группу Users
+                //_logger.LogInformation("Регистрация пользователя {0}", user.UserName);
+                _logger.LogInformation("Регистрация пользователя {UserName}", user.UserName); //можно так
+                                                                                              //_logger.LogInformation($"Регистрация пользователя {user.UserName}"); // так не делать!
+                var regResult = await _userManager.CreateAsync(user, model.Password);
+                if (regResult.Succeeded)
+                {
+                    _logger.LogInformation("Пользователь {0} успешно зарегистрирован", user.UserName);
 
-                return RedirectToAction("Index", "Home");
+                    await _userManager.AddToRoleAsync(user, Role.Users);  //добавляем сразу в группу Users
+                    _logger.LogInformation("Пользователю {0} назначена роль {1}", user.UserName, Role.Users);
+
+                    await _signInManager.SignInAsync(user, false); //false - непостоянный, разовый вход
+                    _logger.LogInformation("Пользователь {0} вошёл в систему после регистрации", user.UserName);
+
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach (var error in regResult.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                _logger.LogWarning("Ошибка при регистрации пользователя {0}: {1}",
+                        user.UserName, string.Join(", ", regResult.Errors.Select(err => err.Description)));
             }
 
-            foreach (var error in regResult.Errors)
-            {
-                ModelState.AddModelError("", error.Description);
-            }
             return View(model);
         }
         #endregion Register
@@ -80,6 +102,7 @@ namespace WebStore.Controllers
 
             if (loginResult.Succeeded)
             {
+                _logger.LogInformation("Пользователь {0} успешно вошёл в систему", model.UserName);
                 //return Redirect(model.ReturnUrl);  //небезопасно! могут подсунуть левый Url и куки утекут на сторону
 
                 //можно так
@@ -94,19 +117,25 @@ namespace WebStore.Controllers
             }
 
             ModelState.AddModelError("", "Ошибка ввода логина или пароля");
+            _logger.LogWarning("Ошибка ввода пользователя, или пароля при входе {0}", model.UserName);
+
             return View(model);
         }
         #endregion Login
 
         public async Task<IActionResult> Logout()
         {
+            var userName = User.Identity!.Name;
             await _signInManager.SignOutAsync();
+            _logger.LogInformation("Пользователь {0} вышел из системы", userName);
             return RedirectToAction("Index", "Home");
         }
 
         [AllowAnonymous]
         public IActionResult AccessDenied()
         {
+            _logger.LogWarning("Пользователю {0} отказано в доступе к uri:{1}",
+                User.Identity!.Name, HttpContext.Request.Path);
             return View();
         }
     }

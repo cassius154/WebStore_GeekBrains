f5cff53a5446e3ab2f29094233798178a8e1ae12
AlexK <cassius154@users.noreply.github.com>
2021-10-26 16:40:00 +0300

    Новый сервис хранения данных корзины

---
+++ Services/WebStore.Interfaces/Services/ICartStore.cs
@@ -0,0 +1,12 @@
+﻿using WebStore.Domain.Entities;
+
+namespace WebStore.Interfaces.Services
+{
+    //производим декомпозицию сервиса Cart
+    //чтобы при тестировании один сервис отвечал исключительно за логику корзины
+    //а другой за всю оболочку (HttpContext, Coockies и т.д.)
+    public interface ICartStore
+    {
+        public Cart Cart { get; set; }
+    }
+}

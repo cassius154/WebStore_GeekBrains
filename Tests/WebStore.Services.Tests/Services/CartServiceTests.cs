01a0dabab6a834ab916592bf577f0d1e4f681240
AlexK <cassius154@users.noreply.github.com>
2021-10-26 16:29:31 +0300

    Тестирование моделей, используемых в корзине

---
+++ Tests/WebStore.Services.Tests/Services/CartServiceTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebStore.Domain.Entities;
+using WebStore.Domain.ViewModels;
+using Assert = Xunit.Assert;
+
+namespace WebStore.Services.Tests.Services
+{
+    [TestClass]
+    public class CartServiceTests
+    {
+        private Cart _cart;
+
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _cart = new Cart
+            {
+                Items = new List<CartItem>
+                {
+                    new() { ProductId = 1, Quantity = 1 },
+                    new() { ProductId = 2, Quantity = 3 },
+                }
+            };
+        }
+
+        [TestMethod]
+        public void CartClassItemsCountReturnsCorrectQuantity()
+        {
+            var cart = _cart;
+
+            var expectedItemsCount = cart.Items.Sum(i => i.Quantity);
+
+            var actualItemsCount = cart.ItemsCount;
+
+            Assert.Equal(expectedItemsCount, actualItemsCount);
+        }
+
+        [TestMethod]
+        public void CartViewModel_Returns_Correct_ItemsCount()
+        {
+            var cartViewModel = new CartViewModel
+            {
+                Items = new[]
+                {
+                    ( new ProductViewModel { Id = 1, Name = "Product 1", Price = 0.5m }, 1 ),
+                    ( new ProductViewModel { Id = 2, Name = "Product 2", Price = 1.5m }, 3 ),
+                }
+            };
+
+            var expectedItemsCount = cartViewModel.Items.Sum(i => i.Quantity);
+
+            var actualItemsCount = cartViewModel.ItemsCount;
+
+            Assert.Equal(expectedItemsCount, actualItemsCount);
+        }
+
+        [TestMethod]
+        public void CartViewModel_Returns_Correct_TotalPrice()
+        {
+            var cartViewModel = new CartViewModel
+            {
+                Items = new[]
+                {
+                    ( new ProductViewModel { Id = 1, Name = "Product 1", Price = 0.5m }, 1 ),
+                    ( new ProductViewModel { Id = 2, Name = "Product 2", Price = 1.5m }, 3 ),
+                }
+            };
+
+            var expectedTotalPrice = cartViewModel.Items.Sum(item => item.Quantity * item.Product.Price);
+
+            var actualTotalPrice = cartViewModel.TotalPrice;
+
+            Assert.Equal(expectedTotalPrice, actualTotalPrice);
+        }
+    }
+}

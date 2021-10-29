eb7e7ac0862ca591db5bc1dc0531c1024cdbc1cd
AlexK <cassius154@users.noreply.github.com>
2021-10-29 21:22:48 +0300

    Логика хлебных крошек

---
+++ UI/WebStore/Components/BreadCrumbsViewComponent.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.ViewModels;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Components
+{
+    public class BreadCrumbsViewComponent : ViewComponent
+    {
+        private readonly IProductService _productService;
+
+        public BreadCrumbsViewComponent(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = new BreadCrumbsViewModel();
+
+            if (int.TryParse(Request.Query["SectionId"], out var sectionId))
+            {
+                model.Section = _productService.GetSectionById(sectionId);
+                if (model.Section.ParentId is { } parentSectionId)
+                    model.Section.Parent = _productService.GetSectionById(parentSectionId);
+            }
+
+            if (int.TryParse(Request.Query["BrandId"], out var brandId))
+            {
+                model.Brand = _productService.GetBrandById(brandId);
+            }
+
+            if (int.TryParse(ViewContext.RouteData.Values["id"]?.ToString(), out var productId))
+            {
+                model.Product = _productService.GetProductById(productId)?.Name;
+            }
+
+            return View(model);
+        }
+    }
+}

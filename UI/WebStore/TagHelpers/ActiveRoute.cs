dfd563f859a53f9e5178c69a3a7d94801dba80ea
AlexK <cassius154@users.noreply.github.com>
2021-10-29 16:06:44 +0300

    Подготовка данных внутри логики таг-хелпера

--- UI/WebStore/TagHelpers/ActiveRoute.cs
+++ UI/WebStore/TagHelpers/ActiveRoute.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace WebStore.TagHelpers
@@ -12,6 +14,23 @@ namespace WebStore.TagHelpers
     public class ActiveRoute : TagHelper
     {
         private const string _attributeName = "ws-is-active-route";
+
+        [HtmlAttributeName("asp-controller")]
+        public string Controller { get; set; }
+
+        [HtmlAttributeName("asp-action")]
+        public string Action { get; set; }
+
+        //так можно получить итоговую генерируемую ссылку
+        //[HtmlAttributeName("href")]
+        //public string Href { get; set; }
+
+        [HtmlAttributeName("asp-all-route-data", DictionaryAttributePrefix = "asp-route-")]
+        public Dictionary<string, string> RouteValues { get; set; } = new(StringComparer.OrdinalIgnoreCase);
+
+        //HtmlAttributeNotBound указывает на то, что свойство ViewContext не связано с разметкой
+        [ViewContext, HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {

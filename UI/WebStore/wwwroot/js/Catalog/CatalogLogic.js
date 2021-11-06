26e68345f18abe69588bd9ef242c26a170928538
AlexK <cassius154@users.noreply.github.com>
2021-11-06 14:21:00 +0300

    Скрипт логики вывода каталога товаров

---
+++ UI/WebStore/wwwroot/js/Catalog/CatalogLogic.js
@@ -0,0 +1,58 @@
+﻿atalog = {
+    _properties: {
+        getViewLink: ""
+    },
+
+    //стрелочные функции - аналог лямбда функций в c#
+    init: properties => {
+        $.extend(Catalog._properties, properties);
+
+        $(".pagination li a").click(Catalog.clickOnPage);
+    },
+
+    clickOnPage: function (e) {
+        e.preventDefault();
+
+        const button = $(this);
+
+        if (button.prop("href").length > 0) {
+            const page = button.data("page");
+
+            const container = $("#catalog-items-container");
+
+            container.LoadingOverlay("show");
+
+            let query = "";
+
+            const data = button.data();
+
+            for (let key in data)
+                if (data.hasOwnProperty(key))
+                    //query += key + "=" + data[key] + "&";
+                    query += `${key}=${data[key]}&`;
+
+            $.get(Catalog._properties.getViewLink + "?" + query)
+                .done(catalogHtml => {
+                    container.html(catalogHtml);
+                    container.LoadingOverlay("hide");
+
+                    //все кнопки делаем неактивными
+                    $(".pagination li").removeClass("active");
+                    $(".pagination li a").prop("href", "#");
+
+
+                    //текущую кнопку делаем активной
+                    //$(`.pagination li a[data-page=${page}]`)
+                    //    .removeAttr("href")
+                    //    .parent().addClass("active");
+                    button
+                        .removeAttr("href")
+                        .parent().addClass("active");
+                })
+                .fail(() => {
+                    container.LoadingOverlay("hide");
+                    console.log("clickOnPage fail");
+                });
+        }
+    }
+}
